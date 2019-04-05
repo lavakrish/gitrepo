6ee035d22e8ea6e067e28412594ac4956bcaee98
lavakrish <49301498+lavakrish@users.noreply.github.com>
2019-04-05 13:10:23 +0530

    adding big3 fact pal and rev files
    calculations

---
+++ reverse.c
@@ -0,0 +1,24 @@
+using System;
+
+static class StringHelper
+{
+    /// <summary>
+    /// Receives string and returns the string with its letters reversed.
+    /// </summary>
+    public static string ReverseString(string s)
+    {
+        char[] arr = s.ToCharArray();
+        Array.Reverse(arr);
+        return new string(arr);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(StringHelper.ReverseString("framework"));
+        Console.WriteLine(StringHelper.ReverseString("samuel"));
+        Console.WriteLine(StringHelper.ReverseString("example string"));
+    }
+}
